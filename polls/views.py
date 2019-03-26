80cedab07dc1e995b42ee3cf78b8338de6ea11ba
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-03-26 22:10:39 +0900

    add details method

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
